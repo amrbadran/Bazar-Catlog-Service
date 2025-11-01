07ac22189c6965550bed99e4d983e2851c385c65
Amr Badran <badran.amr2003@gmail.com>
2025-11-01 17:19:39 +0200

    Add Validator : Add BookStock Validator

---
+++ Bazar-Catlog-Service/Validation/BookStockDTOValidator.cs
@@ -0,0 +1,39 @@
+﻿using Bazar_Catlog_Service.DTOs;
+
+namespace Bazar_Catlog_Service.Validation;
+
+public class BookStockDTOValidator : IValidator
+{
+    private readonly BookStockDTO  _bookStockDTO;
+
+    public BookStockDTOValidator(BookStockDTO bookStockDto)
+    {
+        _bookStockDTO = bookStockDto;
+    }
+
+    public void Validate()
+    {
+        var increae = _bookStockDTO.Increase;
+        var decrease = _bookStockDTO.Decrease;
+
+        if (!increae.HasValue && !decrease.HasValue)
+        {
+            throw new Exception("increae or decrease must have a value");
+        }
+        if (increae > decrease)
+        {
+            throw new Exception("Increase and decrease book stock exceed");
+        }
+
+        if (increae.HasValue && increae.Value < 0)
+        {
+            throw new Exception("Increase is negative");
+        }
+
+        if (decrease.HasValue && decrease.Value < 0)
+        {
+            throw new Exception("Decrease is negative");
+        }
+        
+    }
+}
