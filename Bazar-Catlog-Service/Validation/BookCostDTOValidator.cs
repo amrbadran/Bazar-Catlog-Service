b767ee0798e361d5deac194f0c5b2b439fab1278
Amr Badran <badran.amr2003@gmail.com>
2025-11-01 16:57:59 +0200

    Add Validation For BookCostDTO

---
+++ Bazar-Catlog-Service/Validation/BookCostDTOValidator.cs
@@ -0,0 +1,27 @@
+﻿using Bazar_Catlog_Service.DTOs;
+
+namespace Bazar_Catlog_Service.Validation;
+
+public class BookCostDTOValidator : IValidator
+{
+    private readonly BookCostDTO _bookCostDTO;
+
+    public BookCostDTOValidator(BookCostDTO bookCostDto)
+    {
+        _bookCostDTO = bookCostDto;
+    }
+
+    public void Validate()
+    {
+        var cost = _bookCostDTO.Cost;
+        if (cost == null)
+        {
+            throw new Exception("Book cost is null");
+                
+        }
+        if (cost < 0)
+        {
+            throw new Exception("Book cost is negative");
+        }
+    }
+}
