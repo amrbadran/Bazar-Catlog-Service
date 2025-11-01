8c1a96f32926fc17e18108ec7a6e78aaa3815366
Amr Badran <badran.amr2003@gmail.com>
2025-11-01 18:08:12 +0200

    Add appsettings.json

--- Bazar-Catlog-Service/Program.cs
+++ Bazar-Catlog-Service/Program.cs
@@ -2,11 +2,13 @@
 using Bazar_Catlog_Service.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<BazarDbContext>(options => options.UseSqlite("Data Source=Bazar.db"));
+
+builder.Services.AddDbContext<BazarDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
