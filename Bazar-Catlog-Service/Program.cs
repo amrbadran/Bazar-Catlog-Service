1a64310c1a58c7edc4bace5cba7cfafa611f9ce6
Amr Badran <badran.amr2003@gmail.com>
2025-11-01 18:56:46 +0200

    fix: Seed Data in Program.cs

--- Bazar-Catlog-Service/Program.cs
+++ Bazar-Catlog-Service/Program.cs
@@ -1,16 +1,21 @@
 ﻿using Bazar_Catlog_Service.Api.Books;
 using Bazar_Catlog_Service.Data;
+using Bazar_Catlog_Service.Data.Seeding;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<BazarDbContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// Seed Data
+using var seeding = new SeedBooks(app.Services.GetService<BazarDbContext>());
+seeding.Seed();
 
 app.MapQueryBooksEndpoints();
 app.MapUpdateBooksEndpoints();
